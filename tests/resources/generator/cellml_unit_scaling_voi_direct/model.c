0b200f25dcf419258712ba597db0e5e876657e4d
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-10-10 03:53:45 +1300

    Changes 69.

--- tests/resources/generator/cellml_unit_scaling_voi_direct/model.c
+++ tests/resources/generator/cellml_unit_scaling_voi_direct/model.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 const char VERSION[] = "0.1.0";
-const char LIBCELLML_VERSION[] = "0.2.0";
+const char LIBCELLML_VERSION[] = "0.12.0";
 
 const size_t STATE_COUNT = 2;
 const size_t VARIABLE_COUNT = 0;
