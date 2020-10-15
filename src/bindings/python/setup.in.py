f387807b267064d52843e206853c7db3eeee4a19
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-10-15 15:02:04 +1300

    Changes.

--- src/bindings/python/setup.in.py
+++ src/bindings/python/setup.in.py
@@ -37,7 +37,7 @@ setup(
     author_email='libcellml@googlegroups.com',
     packages=['libcellml'],
     package_data={'libcellml': [@SETUP_PY_PACKAGE_FILES_STR@]},
-    url='http://cellml.org',
+    url='http://libcellml.org',
     license='Apache Software License',
     description=doclines[0],
     classifiers=classifiers.split("\n"),
