7c5b9bb93b3cbf49550de7d2db102790e470dae0
hsorby <h.sorby@auckland.ac.nz>
2021-03-08 23:30:26 +1300

    Clean debug statements from workflow scripts.

--- .github/scripts/upload-asset.js
+++ .github/scripts/upload-asset.js
@@ -3,22 +3,13 @@ const mime = require('mime');
 
 module.exports = ({github, context}) => {
   
-  console.log('=========')
-  //console.log(github)
-  //console.log(context)
-  //console.log(process.env)
-
   let upload_url = context.payload.release.upload_url
   upload_url = upload_url.split('{').shift()
   
-  console.log(process.env.DATA)
-
   const files = process.env.DATA.split(';')
-  console.log(files)  
   for (let index in files) {
     const assetPath = files[index]
     const contentType = mime.getType(assetPath);
-    console.log('mime type:', contentType) 
     const assetName = assetPath.split('/').pop();
     const contentLength = filePath => fs.statSync(filePath).size;
     const headers = { 'content-type': contentType, 'content-length': contentLength(assetPath) };
