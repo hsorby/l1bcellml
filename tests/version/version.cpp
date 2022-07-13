83313ab714bdff8fb04eeee1f0ffabc0a8e57bef
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-07-13 12:53:28 +1200

    Changing version to: 0.3.3-dev.2

--- tests/version/version.cpp
+++ tests/version/version.cpp
@@ -5,8 +5,8 @@
 TEST(Version, versionMatch)
 {
     unsigned int version = libcellml::version();
-    EXPECT_EQ(0x000302U, version);
+    EXPECT_EQ(0x000303U, version);
 
     std::string versionString = libcellml::versionString();
-    EXPECT_EQ("0.3.2", versionString);
+    EXPECT_EQ("0.3.3", versionString);
 }
