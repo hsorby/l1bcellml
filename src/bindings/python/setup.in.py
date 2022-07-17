3a0664593a27aac21dafde4d1beb06c02c7181ab
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-07-18 08:51:24 +1200

    Attempt at fixing linux wheel creation.

--- src/bindings/python/setup.in.py
+++ src/bindings/python/setup.in.py
@@ -17,36 +17,38 @@ Topic :: Software Development :: Libraries :: Python Modules
 """
 
 import os
-from setuptools import setup
+import subprocess
+
+from setuptools import setup, Extension
 from setuptools.dist import Distribution
 from setuptools.command.install import install
+from setuptools.command.build_ext import build_ext
+
 
 doclines = __doc__.split("\n")
 
 
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+# This is a fake module to trick setuptools into creating a wheel.
+_module_name = "libcellml"
 
-    class bdist_wheel(_bdist_wheel):
 
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            # Mark us as not a pure python package
-            self.root_is_pure = False
+class CMakeExtension(Extension):
+    def __init__(self, name, cmake_lists_dir='.', sources=[], **kwa):
+        Extension.__init__(self, name, sources=sources, **kwa)
+        self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
 
-        def get_tag(self):
-            python, abi, plat = _bdist_wheel.get_tag(self)
-            # We don't contain any python source
-            #python, abi = 'py2.py3', 'none'
-            print("=============")
-            print("python:", python)
-            print("abi:", abi)
-            print("plat:", plat)
-            return python, abi, plat
 
-except ImportError:
-    bdist_wheel = None
+class CMakeBuild(build_ext):
 
+    def build_extensions(self):
+        """
+        This doesn't do anything but it fools setuptools into thinking that it does?
+        Manylinux started to install libraries into a purelib folder which didn't pass muster.
+        """
+        for ext in self.extensions:
+
+            extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+       
 
 class BinaryDistribution(Distribution):
     def is_pure(self):
@@ -70,7 +72,10 @@ setup(
     long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
     distclass=BinaryDistribution,
-    cmdclass={'bdist_wheel': bdist_wheel},
+    ext_modules=[CMakeExtension(_module_name)],
+    cmdclass={'build_ext': CMakeBuild},
+    package_data={'libcellml': [@SETUP_PY_PACKAGE_FILES_STR@]},
     include_package_data=True,
     zip_safe=False,
 )
+
